0a345dff4fe8386805b8e868d3c98076c649078d
Jon Higgins <jon@jonhiggins.co.uk>
2023-05-19 16:29:52 +0100

    Add menu scroll away functionality

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "what-input";
 import "url-search-params-polyfill";
 import interUI from "~/fonts/inter-ui-regular.woff2";
@@ -16,6 +16,9 @@ import Transition from "~/components/transition";
 import Typography from "~/components/typography";
 import { useStaticQuery, graphql, PageProps } from "gatsby";
 import favicon from "~/images/favicon.png";
+
+// Accessibility
+import "what-input";
 
 // Source code highlighting CSS
 require("prismjs/themes/prism-tomorrow.css");
@@ -99,13 +102,130 @@ export default function Layout({ children, location }: PageProps) {
     headerFoldUp: false,
     navOpen: false,
   });
+
+  function handleBreakpointChange(e: MediaQueryListEvent) {
+    const isSmallScreen = !e.matches;
+    setState((state) => ({
+      ...state,
+      ...{ breakpointSmall: isSmallScreen },
+    }));
+    // Close the small screen menu when switching to large screen
+    if (!isSmallScreen) {
+      setState((state) => ({
+        ...state,
+        ...{ navOpen: false },
+      }));
+    }
+  }
+
+  useEffect(() => {
+    console.log("mount");
+    if (typeof window === "undefined" || !mediaQueryList) {
+      return;
+    }
+
+    /**
+     * Fix/unfix site header, fold up/down site header when fixed
+     */
+    function fixUnFixHeader(headerFixed: boolean, scrollingUp: boolean) {
+      // Debounce if waiting for requestAnimationFrame
+      memoryStore.set(MEMORY_STORE_KEYS.WAITING_FOR_ANIMATION_FRAME, false);
+
+      if (state.navOpen) {
+        return;
+      }
+
+      setState((state) => ({
+        ...state,
+        ...{
+          headerFixed,
+          headerFoldUp: headerFixed,
+        },
+      }));
+
+      if (headerFixed && scrollingUp) {
+        setState((state) => ({
+          ...state,
+          ...{ headerFoldUp: false },
+        }));
+      }
+    }
+
+    function handleScroll() {
+      console.log("handle scroll");
+      const { scrollY } = window;
+      const scrollingUp =
+        scrollY < memoryStore.get(MEMORY_STORE_KEYS.LAST_SCROLL_Y);
+      memoryStore.set(MEMORY_STORE_KEYS.LAST_SCROLL_Y, scrollY);
+
+      if (memoryStore.get(MEMORY_STORE_KEYS.WAITING_FOR_ANIMATION_FRAME)) {
+        return;
+      }
+
+      const scrollTriggerPoint = state.breakpointSmall
+        ? HEADER_HEIGHT_S
+        : HEADER_HEIGHT_M;
+      const headerFixed = scrollY > scrollTriggerPoint;
+      window.requestAnimationFrame(
+        fixUnFixHeader.bind(null, headerFixed, scrollingUp)
+      );
+
+      memoryStore.set(MEMORY_STORE_KEYS.WAITING_FOR_ANIMATION_FRAME, true);
+    }
+
+    // Set memory store inital values
+    memoryStore.set(MEMORY_STORE_KEYS.WAITING_FOR_ANIMATION_FRAME, false);
+    memoryStore.set(MEMORY_STORE_KEYS.LAST_SCROLL_Y, 0);
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Set up media query listeners (header different height on big screen)
+    setState((state) => ({
+      ...state,
+      ...{ breakpointSmall: !mediaQueryList.matches },
+    }));
+    mediaQueryList.addEventListener("change", handleBreakpointChange);
+
+    // Clean-up on unmount component
+    return function () {
+      console.log("unmount");
+      // No window in server rendered version
+      if (typeof window === "undefined") {
+        return;
+      }
+      window.removeEventListener("scroll", handleScroll);
+      mediaQueryList.removeEventListener("change", handleBreakpointChange);
+    };
+  }, [state.breakpointSmall, state.navOpen]);
+
+  /**
+   * Toggle small screen menu
+   */
+  function handleNavToggleClick() {
+    setState((state) => ({ ...state, ...{ navOpen: !state.navOpen } }));
+    if (!state.navOpen) {
+      return;
+    }
+    // Nav is open so unfix and fold down the menu
+    setState((state) => ({
+      ...state,
+      ...{
+        headerFixed: false,
+        headerFoldUp: false,
+      },
+    }));
+  }
+  /**
+   * Click on link in nav to close small screen menu
+   */
+  function handleMenuClick() {
+    setState((state) => ({ ...state, ...{ navOpen: false } }));
+  }
+
   const search = location ? location.search : null;
   const params = search ? new URLSearchParams(search) : null;
   const grid = params ? params.get("grid") : false;
   const hasGrid = grid === "true" || grid === "1";
-
-  function handleNavToggleClick() {}
-  function handleMenuClick() {}
   return (
     <Wrapper headerFixed={state.headerFixed}>
       <Helmet title={siteData.site.siteMetadata.title}>
