e33309306ddc8b3d92f45b07f7e1b93a4447b2f2
Jon Higgins <jon@jonhiggins.co.uk>
2023-07-17 19:11:54 +0100

    Add sitemap

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -28,6 +28,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    `gatsby-plugin-sitemap`,
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
     "gatsby-plugin-layout",
